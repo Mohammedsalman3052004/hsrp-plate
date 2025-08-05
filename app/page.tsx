55d51c68f4318a3f89bdcba09d368414fb6c8e45
Mohammedsalman3052004 <mdsalman3052004@gmail.com>
2025-08-05 14:34:55 +0530

    Price Updated

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
       <HomeCtc />
       <div className='floating-icon'>
         <a href="https://wa.me/+919443447469" target="_blank" rel="noopener noreferrer">
-          <img src="/images/whatsapp-icon.webp" alt="WhatsApp Icon" className='whatsapp-icon icon' />
+          <img src="/images/WhatsApp-icon.webp" alt="WhatsApp Icon" className='whatsapp-icon icon' />
         </a>
         <a href="tel:9443447469" target="_blank" rel="noopener noreferrer">
           <img src="/images/call-icon.webp" alt="call Icon" className='call-icon icon' />
